7d05eb6898657268028f2169d5680c1be612bd25
Honza Dvorsky <jan.dvorsky@yahoo.com>
2016-08-24 08:32:13 +0200

    Use Zewo/COpenSSL

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "TLS",
     dependencies: [
-    	.Package(url: "https://github.com/PerfectlySoft/Perfect-COpenSSL.git", majorVersion: 0, minor: 7),
+    	.Package(url: "https://github.com/Zewo/COpenSSL.git", majorVersion: 0, minor: 8),
     ]
 )
