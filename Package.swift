5a083d7972af67e4fe1a8599d1a0dda61e8787f5
Logan Wright <logan.william.wright@gmail.com>
2016-08-24 23:39:13 -0400

    update dep

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "TLS",
     dependencies: [
-    	.Package(url: "https://github.com/vapor/clibressl.git", Version(0,1,9)),
+    	.Package(url: "https://github.com/vapor/clibressl.git", majorVersion: 0, minor: 1),
     ]
 )
