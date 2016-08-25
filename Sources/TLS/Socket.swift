ada764aecee1cdc0c8218eb20a26bfb215ad8840
Tanner Nelson <me@tanner.xyz>
2016-08-25 01:05:25 -0400

    new tls

--- Sources/TLS/Socket.swift
+++ Sources/TLS/Socket.swift
@@ -4,33 +4,24 @@ import CLibreSSL
     An SSL Socket.
 */
 public final class Socket {
-    public typealias CSSL = UnsafeMutablePointer<ssl_st>
-
-    public let cSSL: CSSL
-    public let context: Context
     public let descriptor: Int32
+
+    public let cConfig: OpaquePointer
+    public let cContext: OpaquePointer
+
 
     /**
         Indicates whether or not an ssl connection received a shutdown.
     */
     public var closed: Bool {
-        let shutDown = SSL_get_shutdown(cSSL)
-        switch shutDown {
-        case 0:
-            return false
-        case SSL_SENT_SHUTDOWN, SSL_RECEIVED_SHUTDOWN:
-            return true
-        default:
-            return true
-        }
+        return false // FIXME
     }
 
     /**
          The current timeout associated with the socket.
     */
     public var timeout: Int {
-        let session = SSL_get_session(cSSL)
-        return SSL_SESSION_get_time(session)
+        return 0 // FIXME
     }
 
     /**
@@ -41,19 +32,22 @@ public final class Socket {
          - parameter descriptor: The file descriptor from an unsecure socket already created.
     */
     public init(context: Context, descriptor: Int32) throws {
-        guard let ssl = SSL_new(context.cContext) else {
-            throw TLSError.socketCreation(error)
+        tls_init()
+        cConfig = tls_config_new()
+
+        switch context.mode {
+        case .server:
+            cContext = tls_server()
+        case .client:
+            cContext = tls_client()
         }
 
-        SSL_set_fd(ssl, descriptor)
-        self.context = context
-        self.cSSL = ssl
         self.descriptor = descriptor
     }
 
     deinit {
-        SSL_shutdown(cSSL)
-        SSL_free(cSSL)
+        tls_free(cContext)
+        tls_config_free(cConfig)
     }
 
 
@@ -61,9 +55,7 @@ public final class Socket {
         Set a timeout for a given connection, in Seconds.
     */
     public func setTimeout(_ timeout: Int) throws {
-        let session = SSL_get_session(cSSL)
-        let result = SSL_SESSION_set_time(session, timeout)
-        guard result != -1 else { throw TLSError.setTimeout(SocketError(Int32(result)), error) }
+        // FIXME
     }
 
     /**
@@ -71,8 +63,8 @@ public final class Socket {
 
          This should only be called if the Context's mode is `.client`
     */
-    public func connect() throws {
-        let result = SSL_connect(cSSL)
+    public func connect(servername: String) throws {
+        let result = tls_connect_socket(cContext, descriptor, servername)
         guard result == Result.OK else {
             throw TLSError.connect(SocketError(result), error)
         }
@@ -84,7 +76,7 @@ public final class Socket {
          This should only be called if the Context's mode is `.server`
     */
     public func accept() throws {
-        let result = SSL_accept(cSSL)
+        let result = tls_accept_socket(cContext, nil, descriptor)
         guard result == Result.OK else {
             throw TLSError.accept(SocketError(result), error)
         }
@@ -101,11 +93,11 @@ public final class Socket {
             pointer.deallocate(capacity: max)
         }
 
-        let result = SSL_read(cSSL, pointer, max.int32)
+        let result = tls_read(cContext, pointer, max)
         let bytesRead = Int(result)
 
         guard bytesRead >= 0 else {
-            throw TLSError.receive(SocketError(result), error)
+            throw TLSError.receive(SocketError(result.int32), error)
         }
 
 
@@ -121,10 +113,10 @@ public final class Socket {
     public func send(_ bytes: [UInt8]) throws {
         let buffer = UnsafeBufferPointer<UInt8>(start: bytes, count: bytes.count)
 
-        let bytesSent = SSL_write(cSSL, buffer.baseAddress, bytes.count.int32)
+        let bytesSent = tls_write(cContext, buffer.baseAddress, bytes.count)
 
         guard bytesSent >= 0 else {
-            throw TLSError.send(SocketError(bytesSent), error)
+            throw TLSError.send(SocketError(bytesSent.int32), error)
         }
     }
 
@@ -132,40 +124,7 @@ public final class Socket {
         Sends a shutdown to secure socket
     */
     public func close() throws {
-        let result = SSL_shutdown(cSSL)
+        let result = tls_close(cContext)
         guard result != -1 else { throw TLSError.close(SocketError(result), error) }
-    }
-
-    /**
-         Verifies the connection with the peer.
-
-         - throws: TLSError.invalidPeerCertificate(PeerCertificateError)
-    */
-    public func verifyConnection() throws {
-        if
-            case.server = context.mode,
-            context.certificates.areSelfSigned
-        {
-            return
-        }
-
-        guard let certificate = SSL_get_peer_certificate(cSSL) else {
-            throw TLSError.invalidPeerCertificate(.notPresented)
-        }
-        defer {
-            X509_free(certificate)
-        }
-
-        let result = SSL_get_verify_result(cSSL).int32
-        switch result {
-        case X509_V_OK:
-            break
-        case X509_V_ERR_UNABLE_TO_GET_ISSUER_CERT, X509_V_ERR_UNABLE_TO_GET_ISSUER_CERT_LOCALLY:
-            if !context.certificates.areSelfSigned {
-                throw TLSError.invalidPeerCertificate(.noIssuerCertificate)
-            }
-        default:
-            throw TLSError.invalidPeerCertificate(.invalid)
-        }
     }
 }
--- Tests/TLSTests/ContextTests.swift
+++ Tests/TLSTests/ContextTests.swift
@@ -1,38 +1,12 @@
 import XCTest
 @testable import TLS
 
-#if Xcode
-var workDir: String {
-    let parent = #file.characters.split(separator: "/").map(String.init).dropLast().joined(separator: "/")
-    let path = "/\(parent)/../../"
-    return path
-}
-#else
-let workDir = "./"
-#endif
 class ContextTests: XCTestCase {
     static var allTests = [
-        ("testInitServer", testInitServer),
-        ("testInitClient", testInitClient)
+        ("testBasic", testBasic)
     ]
 
-    func testInitServer() {
-        do {
-            _ = try TLS.Context(mode: .server, certificates: .files(
-                certificateFile: workDir + "Certs/cert.pem",
-                privateKeyFile: workDir + "Certs/key.pem",
-                signature: .selfSigned
-            ))
-        } catch {
-            XCTFail("Initialization failed: \(error)")
-        }
-    }
+    func testBasic() {
 
-    func testInitClient() {
-        do {
-            _ = try TLS.Context(mode: .client, certificates: .none)
-        } catch {
-            XCTFail("Initialization failed: \(error)")
-        }
     }
 }
