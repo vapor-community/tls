8392c65d860ef89d5fa8cb2dccc8893c3ddb7c70
Logan Wright <logan.william.wright@gmail.com>
2016-10-31 13:28:40 -0400

    update to socks 1.1

--- Package.swift
+++ Package.swift
@@ -5,6 +5,6 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/vapor/clibressl.git", majorVersion: 1),
         .Package(url: "https://github.com/vapor/core.git", majorVersion: 1),
-      	.Package(url: "https://github.com/vapor/socks.git", majorVersion: 1),
+      	.Package(url: "https://github.com/vapor/socks.git", versions: Version(1,1,0)..<Version(2,0,0)),
     ]
 )
