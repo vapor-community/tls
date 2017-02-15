bbb52d9c1437ef7f2fe0e2bb942b02e968189b91
Logan Wright <logan.william.wright@gmail.com>
2017-02-15 13:16:47 +0100

    alpha core

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
     name: "TLS",
     dependencies: [
         .Package(url: "https://github.com/vapor/clibressl.git", majorVersion: 1),
-        .Package(url: "https://github.com/vapor/core.git", majorVersion: 1),
+        .Package(url: "https://github.com/vapor/core.git", Version(2,0,0, prereleaseIdentifiers: ["alpha"])),
       	.Package(url: "https://github.com/vapor/socks.git", versions: Version(1,1,0)..<Version(2,0,0)),
     ]
 )
