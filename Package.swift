94f1b9804de5709ca6831bb254646f4304fff02f
Tanner Nelson <me@tanner.xyz>
2017-03-08 12:13:04 +0100

    use vapor ctls

--- Package.swift
+++ Package.swift
@@ -5,6 +5,6 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/vapor/core.git", Version(2,0,0, prereleaseIdentifiers: ["alpha"])),
       	.Package(url: "https://github.com/vapor/socks.git", Version(2,0,0, prereleaseIdentifiers: ["alpha"])),
-        .Package(url: "https://github.com/tanner0101/ctls.git", majorVersion: 0)
+        .Package(url: "https://github.com/vapor/ctls.git", majorVersion: 0)
     ]
 )
