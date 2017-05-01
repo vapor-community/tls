62ff9d15de500a903a34ae961e72dcf9c3d78a37
tanner0101 <me@tanner.xyz>
2017-05-01 11:46:48 +0200

    sockets buffer update

--- Sources/TLS/ReadableSocket.swift
+++ Sources/TLS/ReadableSocket.swift
@@ -6,14 +6,8 @@ extension ReadableSocket {
     /// Receives bytes from the secure socket.
     ///
     /// - parameter max: The maximum amount of bytes to receive.
-    public func read(max: Int) throws -> Bytes  {
-        let pointer = UnsafeMutablePointer<Byte>
-            .allocate(capacity: max)
-        defer {
-            pointer.deallocate(capacity: max)
-        }
-
-        let bytesRead = SSL_read(cSSL, pointer, Int32(max))
+    public func read(max: Int, into buffer: inout Bytes) throws -> Int  {
+        let bytesRead = SSL_read(cSSL, &buffer, Int32(max))
 
         if bytesRead <= 0 {
             throw makeError(
@@ -22,10 +16,6 @@ extension ReadableSocket {
             )
         }
 
-        let buffer = UnsafeBufferPointer<Byte>.init(
-            start: pointer,
-            count: Int(bytesRead)
-        )
-        return Array(buffer)
+        return Int(bytesRead)
     }
 }
