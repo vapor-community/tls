eba18538ebc13dbb37021c3dea72f73db2bba7eb
Logan Wright <logan.william.wright@gmail.com>
2017-08-04 15:25:00 +0200

    update swift 4 manifest

---
+++ Package@swift-4.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:4.0
+import PackageDescription
+
+let package = Package(
+    name: "TLS",
+    products: [
+        .library(name: "TLS", targets: ["TLS"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/vapor/core.git", .upToNextMajor(from: "2.1.1")),
+      	.package(url: "https://github.com/vapor/sockets.git", .upToNextMajor(from: "2.1.0")),
+        .package(url: "https://github.com/vapor/ctls.git", .upToNextMajor(from: "1.0.0")),
+    ],
+    targets: [
+        .target(name: "TLS", dependencies: ["Core", "Sockets", "CTLS"]),
+        .testTarget(name: "TLSTests", dependencies: ["TLSTests"]),
+    ]
+)
