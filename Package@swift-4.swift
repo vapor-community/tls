c6178768312d1f354ea3c1cb3916534b6a1a5a36
Logan Wright <logan.william.wright@gmail.com>
2017-08-04 15:29:44 +0200

    fix manifest

--- Package@swift-4.swift
+++ Package@swift-4.swift
@@ -13,6 +13,6 @@ let package = Package(
     ],
     targets: [
         .target(name: "TLS", dependencies: ["Core", "Sockets", "CTLS"]),
-        .testTarget(name: "TLSTests", dependencies: ["TLSTests"]),
+        .testTarget(name: "TLSTests", dependencies: ["TLS"]),
     ]
 )
